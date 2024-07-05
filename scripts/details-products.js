dad88751564a3fffe269ae7dbe2bbd67b2f4b67d
Sara Jaramillo <sarasunrise45@gmail.com>
2024-07-05 17:41:53 -0500

    Se añade función para seleccionar el tamaño de los productos

--- scripts/details-products.js
+++ scripts/details-products.js
@@ -41,3 +41,20 @@ document.getElementById("menuButton").addEventListener("click", function () {
     navbar.classList.add("active");
   }
 });
+
+//Seleccionar tamaño del producto
+document.addEventListener('DOMContentLoaded', function () {
+  const sizeOptions = document.querySelectorAll('.size-options .size');
+
+  sizeOptions.forEach(function (sizeOption) {
+      sizeOption.addEventListener('click', function () {
+          // Remove the 'selected' class from all size options
+          sizeOptions.forEach(function (option) {
+              option.classList.remove('selected');
+          });
+
+          // Add the 'selected' class to the clicked size option
+          this.classList.add('selected');
+      });
+  });
+});
