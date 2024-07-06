0ec11b1dd66585c4b982c6091c885c02121869d2
Sara Jaramillo <sarasunrise45@gmail.com>
2024-07-05 19:53:45 -0500

    Se agrega función para que el breadcrumb se actualice dinámicamente

--- scripts/details-products.js
+++ scripts/details-products.js
@@ -43,18 +43,54 @@ document.getElementById("menuButton").addEventListener("click", function () {
 });
 
 //Seleccionar tamaño del producto
-document.addEventListener('DOMContentLoaded', function () {
-  const sizeOptions = document.querySelectorAll('.size-options .size');
+document.addEventListener("DOMContentLoaded", function () {
+  const sizeOptions = document.querySelectorAll(".size-options .size");
 
   sizeOptions.forEach(function (sizeOption) {
-      sizeOption.addEventListener('click', function () {
-          // Remove the 'selected' class from all size options
-          sizeOptions.forEach(function (option) {
-              option.classList.remove('selected');
-          });
-
-          // Add the 'selected' class to the clicked size option
-          this.classList.add('selected');
+    sizeOption.addEventListener("click", function () {
+      sizeOptions.forEach(function (option) {
+        option.classList.remove("selected");
       });
+
+      this.classList.add("selected");
+    });
   });
+});
+
+//Aumentar y disminuir la cantidad de los productos
+document.addEventListener("DOMContentLoaded", function () {
+  const productDetails = document.querySelectorAll(".product-detail");
+
+  productDetails.forEach(function (productDetail) {
+    const decreaseButton = productDetail.querySelector(".quantity-decrease");
+    const increaseButton = productDetail.querySelector(".quantity-increase");
+    const quantityInput = productDetail.querySelector('input[type="number"]');
+
+    decreaseButton.addEventListener("click", function () {
+      let currentValue = parseInt(quantityInput.value);
+      if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+      }
+    });
+
+    increaseButton.addEventListener("click", function () {
+      let currentValue = parseInt(quantityInput.value);
+      quantityInput.value = currentValue + 1;
+    });
+  });
+});
+
+//Mostra el nav de migas de pan dinamicamente
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtener el identificador del producto desde la URL
+  let productId = window.location.hash;
+
+  productId = productId.substring(1);
+
+  let breadcrumbSpan = document.getElementById("current-product");
+  breadcrumbSpan.textContent = document
+    .getElementById(productId)
+    .querySelector(".product-name").textContent;
+
+  console.log("Product ID:", productId);
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 //Activar y desactivar el menu
 document.getElementById("menuButton").addEventListener("click", function () {
-    var navbar = document.getElementById("navbar");
-    if (navbar.classList.contains("active")) {
-      navbar.classList.remove("active");
-    } else {
-      navbar.classList.add("active");
-    }
-  });
+  var navbar = document.getElementById("navbar");
+  if (navbar.classList.contains("active")) {
+    navbar.classList.remove("active");
+  } else {
+    navbar.classList.add("active");
+  }
+});
